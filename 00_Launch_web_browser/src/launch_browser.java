224b6c45270bb7675a3207607624dafe45e1b5e9
unknown <a21310422@ceti.mx>
2021-09-16 07:02:09 -0500

    Updated webdrivers path

--- 00_Launch_web_browser/src/launch_browser.java
+++ 00_Launch_web_browser/src/launch_browser.java
@@ -4,24 +4,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class launch_browser
 {
     public static void main ( String [] args ){
-        System.setProperty( "webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe" );
+        System.setProperty( "webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe" );
         WebDriver driver = new ChromeDriver();
 
         String title = "";
         String expectedTitle = "Google";
         String url = "https://www.google.com.mx/";
         driver.get( url );
-
-        title = driver.getTitle();
-
-        if ( expectedTitle.contentEquals( title ) )
-        {
-            System.out.println("Pass...");
-        }
-        else
-        {
-            System.out.println( "Fail...");
-        }
 
         driver.close();
     }
