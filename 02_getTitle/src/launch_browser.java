f33f3fc5077433a0b4a02e239eaca6e7cd926c46
unknown <a21310422@ceti.mx>
2021-09-16 06:48:54 -0500

    Example to check website title

---
+++ 02_getTitle/src/launch_browser.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class launch_browser
+{
+    public static void main ( String [] args ){
+        System.setProperty( "webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe" );
+        WebDriver driver = new ChromeDriver();
+
+        String title = "";
+        String expectedTitle = "Google";
+        String url = "https://www.google.com.mx/";
+        driver.get( url );
+
+        title = driver.getTitle();
+
+        if ( expectedTitle.contentEquals( title ) )
+        {
+            System.out.println("Pass...");
+        }
+        else
+        {
+            System.out.println( "Fail...");
+        }
+
+        driver.close();
+    }
+}
